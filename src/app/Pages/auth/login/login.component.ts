a29850e208c6355fd30ebd9fc1e0c487d31a030c
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-14 16:32:43 -0800

    feat/form validators

--- src/app/Pages/auth/login/login.component.ts
+++ src/app/Pages/auth/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(private fb: FormBuilder){}
+
+  loginForm = this.fb.group({
+    email:['', Validators.required, Validators.email],
+    password: ['', Validators.required]
+  })
+
 
 }
--- src/app/Pages/auth/register/register.component.ts
+++ src/app/Pages/auth/register/register.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  constructor(private fb: FormBuilder){}
+
+  registerForm = this.fb.group({
+    email:['', Validators.required, Validators.email],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  })
 
 }
