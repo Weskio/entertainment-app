bd988144d6621c1aaeb55aeed9dcc0da6958673c
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-15 16:17:20 -0800

    feat/ movies page

--- src/app/Pages/movies/movies.component.ts
+++ src/app/Pages/movies/movies.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
+import { TrendingCardComponent } from "../../components/trending-card/trending-card.component";
+import { RegularCardComponent } from "../../components/regular-card/regular-card.component";
 
 @Component({
-  selector: 'app-movies',
-  standalone: true,
-  imports: [],
-  templateUrl: './movies.component.html',
-  styleUrl: './movies.component.css'
+    selector: 'app-movies',
+    standalone: true,
+    templateUrl: './movies.component.html',
+    styleUrl: './movies.component.css',
+    imports: [SidebarComponent, SearchBarComponent, TrendingCardComponent, RegularCardComponent]
 })
 export class MoviesComponent {
 
