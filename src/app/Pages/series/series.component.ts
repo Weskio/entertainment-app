88a7a53edd2a204eba8d1578dea714c822d59483
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-15 16:29:06 -0800

    feat/ series page

--- src/app/Pages/series/series.component.ts
+++ src/app/Pages/series/series.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { RegularCardComponent } from "../../components/regular-card/regular-card.component";
 
 @Component({
-  selector: 'app-series',
-  standalone: true,
-  imports: [],
-  templateUrl: './series.component.html',
-  styleUrl: './series.component.css'
+    selector: 'app-series',
+    standalone: true,
+    templateUrl: './series.component.html',
+    styleUrl: './series.component.css',
+    imports: [SearchBarComponent, SidebarComponent, RegularCardComponent]
 })
 export class SeriesComponent {
 
