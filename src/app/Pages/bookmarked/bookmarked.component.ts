e546e5f9ba103ab34da161b9d43a0de73eb6ba58
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-16 08:51:44 -0800

    feat/ bookmarked page + routing

--- src/app/Pages/bookmarked/bookmarked.component.ts
+++ src/app/Pages/bookmarked/bookmarked.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
+import { RegularCardComponent } from "../../components/regular-card/regular-card.component";
 
 @Component({
-  selector: 'app-bookmarked',
-  standalone: true,
-  imports: [],
-  templateUrl: './bookmarked.component.html',
-  styleUrl: './bookmarked.component.css'
+    selector: 'app-bookmarked',
+    standalone: true,
+    templateUrl: './bookmarked.component.html',
+    styleUrl: './bookmarked.component.css',
+    imports: [SidebarComponent, SearchBarComponent, RegularCardComponent]
 })
 export class BookmarkedComponent {
 
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgClass, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  @Input() activePage!:string
 
 }
